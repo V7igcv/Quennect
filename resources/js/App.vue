a82cb17e5a4cfd008b23fa7d346b1a163c36344b
V7igcv <igcv.974@gmail.com>
2026-03-03 11:50:05 +0800

    Initial Project Setup

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1 class="text-3xl font-bold text-blue-600">
+            Welcome to Quennect!
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
