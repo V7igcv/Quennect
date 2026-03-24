2ef27ff412469a8e40270cc2c0005b5bce14f358
V7igcv <igcv.974@gmail.com>
2026-03-24 12:16:39 +0800

    step 3: register twilio driver in sms sender manager

--- app/Services/Sms/SmsSenderManager.php
+++ app/Services/Sms/SmsSenderManager.php
@@ -10,6 +10,7 @@ class SmsSenderManager
     {
         return match ($driver ?? config('sms.driver', 'null')) {
             'smsapiph' => new SmsApiPhSmsSender(),
+            'twilio' => new TwilioSmsSender(),
             'semaphore' => new SemaphoreSmsSender(),
             'null' => new NullSmsSender(),
             default => new NullSmsSender(),
